52ba78af14cf61f02f1fe94e289f3ea40e868b0e
Mikołaj Wiszniewski <mikowisz@gmail.com>
2025-03-08 10:44:30 +0100

    feat(lab3): zadanie 1, ale na readString, zamiast read

---
+++ Lab3/zad1.ino
@@ -0,0 +1,40 @@
+#include <LiquidCrystal.h>
+
+LiquidCrystal LCD(12,11,5,4,3,2);
+String text = "";
+
+byte mesh[8] = { 0b10101, 0b01010, 0b10101, 0b01010, 0b10101, 0b01010, 0b10101, 0b01010 };
+
+void readString(){
+  if(Serial.available() > 0){
+    text = Serial.readStringUntil('\n');
+    if(text.length() > 16){
+      Serial.println("Too many chars.");
+    }
+  }
+}
+
+void writeString(){
+  if(text.length()){
+    LCD.clear();
+    for(int i = 0; i < text.length(); i++){
+      LCD.write(text[i]);
+    }
+    LCD.write("\1");
+    text="";
+  }
+}
+
+void setup(){
+  LCD.begin(16,2);
+  LCD.clear();
+  LCD.setCursor(0,0);
+  Serial.begin(9600);
+  LCD.createChar(1, mesh);
+}
+
+void loop(){
+  readString();
+  writeString();
+}
+X
